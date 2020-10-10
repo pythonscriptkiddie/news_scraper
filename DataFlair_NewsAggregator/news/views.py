ea2723a7ca8c760391df12b7691e286f01550810
johndzervos <ioannis@crunchrapps.com>
2020-10-10 15:25:27 +0200

    Completed the tutorial

---
+++ DataFlair_NewsAggregator/news/views.py
@@ -0,0 +1,33 @@
+import requests
+from django.shortcuts import render, redirect
+from bs4 import BeautifulSoup as BSoup
+from news.models import Headline
+
+def scrape(request):
+  session = requests.Session()
+  session.headers = {"User-Agent": "Googlebot/2.1 (+http://www.google.com/bot.html)"}
+  url = "https://www.theonion.com/"
+  content = session.get(url, verify=False).content
+  soup = BSoup(content, "html.parser")
+  News = soup.find_all('article', {"class":"js_post_item"})
+  print("SCRAPE")
+  print("NEWS: ", News)
+  for article in News:
+    main = article.find_all('a')[0]
+    link = main['href']
+    image_src = '' # str(main.find('img')['srcset']).split(" ")[-4]
+    title = 'test' #main['title']
+
+    new_headline = Headline()
+    new_headline.title = title
+    new_headline.url = link
+    new_headline.image = image_src
+    new_headline.save()
+  return redirect("../")
+
+def news_list(request):
+  headlines = Headline.objects.all()[::-1]
+  context = {
+      'object_list': headlines,
+  }
+  return render(request, "news/home.html", context)
