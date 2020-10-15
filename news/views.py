63e76ec3bae734560350d36b3ef90c3322caf933
johndzervos <ioannis@crunchrapps.com>
2020-10-16 00:10:14 +0200

    Improve scraping

--- news/views.py
+++ news/views.py
@@ -3,26 +3,41 @@ from django.shortcuts import render, redirect
 from bs4 import BeautifulSoup as BSoup
 from news.models import Headline
 
-def scrape(request):
-  session = requests.Session()
-  session.headers = {"User-Agent": "Googlebot/2.1 (+http://www.google.com/bot.html)"}
-  url = "https://www.theonion.com/"
+SITES_MAPPING = [
+  {
+    'url': "https://www.theonion.com/",
+    'div_selector': 'article',
+    'div_class': 'js_post_item'
+  }
+]
+
+def get_news(session, url, div_selector, div_class):
   content = session.get(url, verify=False).content
   soup = BSoup(content, "html.parser")
-  News = soup.find_all('article', {"class":"js_post_item"})
-  print("SCRAPE")
-  print("NEWS: ", len(News))
-  for article in News:
+  return soup.find_all(div_selector, {"class": div_class})
+
+def save_news(news):
+  for article in news:
     main = article.find_all('a')[0]
     link = main['href']
     image_src = '' # str(main.find('img')['srcset']).split(" ")[-4]
-    title = 'test2' #main['title']
+    title = 'test' # main['title']
 
     new_headline = Headline()
     new_headline.title = title
     new_headline.url = link
     new_headline.image = image_src
     new_headline.save()
+
+def scrape(request):
+  session = requests.Session()
+  session.headers = {"User-Agent": "Googlebot/2.1 (+http://www.google.com/bot.html)"}
+
+  for source in SITES_MAPPING:
+    news = get_news(session, source['url'], source['div_selector'], source['div_class'])
+    print("SCRAPE: ", source['url'])
+    print("NEWS: ", len(news))
+    save_news(news)
   return redirect("../")
 
 def delete_all(request):
