fa993ce1d02743eed739c6b20a280271f696fa16
johndzervos <ioannis@crunchrapps.com>
2020-10-18 22:51:46 +0200

    Scrape bbc news and do not save duplicates

--- news/views.py
+++ news/views.py
@@ -4,10 +4,15 @@ from bs4 import BeautifulSoup as BSoup
 from news.models import Headline
 
 SITES_MAPPING = [
+  # {
+  #   'url': "https://www.theonion.com/",
+  #   'div_selector': 'article',
+  #   'div_class': 'js_post_item'
+  # },
   {
-    'url': "https://www.theonion.com/",
-    'div_selector': 'article',
-    'div_class': 'js_post_item'
+    'url': 'https://www.bbc.com/',
+    'div_selector': 'div',
+    'div_class': 'media',
   }
 ]
 
@@ -16,18 +21,28 @@ def get_news(session, url, div_selector, div_class):
   soup = BSoup(content, "html.parser")
   return soup.find_all(div_selector, {"class": div_class})
 
-def save_news(news):
+def save_news(source_url, news):
   for article in news:
-    main = article.find_all('a')[0]
-    link = main['href']
-    image_src = '' # str(main.find('img')['srcset']).split(" ")[-4]
-    title = 'test' # main['title']
+    print("======", article)
+    title = article.find_all('a', {"class": 'media__link'})
+    print("TITLE: ", title)
+    if len(title) > 0:
+      title = title[0].contents[0]
+    else:
+      title = "NO TITLE"
+    image_src = article.find('img')['src']
+    print("IMAGE: ", image_src)
+    href = source_url[:-1] + article.find('a')['href']
+    print("HREF: ", href)
 
-    new_headline = Headline()
-    new_headline.title = title
-    new_headline.url = link
-    new_headline.image = image_src
-    new_headline.save()
+    fields = {
+      'title': title,
+      'url': href,
+      'image': image_src
+    }
+
+    # Do not save duplicates
+    new_headline = Headline.objects.get_or_create(**fields)
 
 def scrape(request):
   session = requests.Session()
@@ -37,7 +52,7 @@ def scrape(request):
     news = get_news(session, source['url'], source['div_selector'], source['div_class'])
     print("SCRAPE: ", source['url'])
     print("NEWS: ", len(news))
-    save_news(news)
+    save_news(source['url'], news)
   return redirect("../")
 
 def delete_all(request):
@@ -46,7 +61,7 @@ def delete_all(request):
   return redirect("../")
 
 def news_list(request):
-  headlines = Headline.objects.all()[::-1]
+  headlines = Headline.objects.all()
   context = {
       'object_list': headlines,
   }
