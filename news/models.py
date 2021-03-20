a1cd209e021dce9ff69bb51dd5b2c64ff03081be
Thomas Sullivan <thomaswsullivan@gmail.com>
2021-03-20 07:14:01 -0400

    models recreated

--- news/models.py
+++ news/models.py
@@ -1,8 +1,23 @@
 from django.db import models
+
+class Publication(models.Model):
+  homepage_url = models.URLField(unique=True)
+
+  def __str__(self):
+    return self.homepage_url
+
 class Headline(models.Model):
   title = models.CharField(max_length=200)
   # source = models.CharField(max_length=200)
   image = models.URLField(null=True, blank=True)
   url = models.TextField()
+  publication = models.ForeignKey(
+    Publication,
+    related_name='headlines',
+    on_delete=models.SET_NULL,
+    blank=True,
+    null=True
+  )
+
   def __str__(self):
     return self.title
